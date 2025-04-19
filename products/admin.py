82849cc76081113fb5840e74254dc1c41f50436b
310chowdhury <162335771+310chowdhury@users.noreply.github.com>
2025-04-19 20:07:34 +0600

    Delete products directory

--- products/admin.py
+++
@@ -1,8 +0,0 @@
-# products/admin.py
-from django.contrib import admin
-from .models import Product
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'created_at', 'updated_at')
-    search_fields = ('name', 'description')
